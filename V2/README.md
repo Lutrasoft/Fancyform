67bcc58db63fc598fd04a87bff2aaf9dc1509216
Niels den Otter <tsukasa1989@gmail.com>
2013-07-29 21:43:01 +0200

    2.0.1A Basic radio/checkbox ready, started SELECT

--- V2/README.md
+++ V2/README.md
@@ -5,3 +5,16 @@
 This version is just a start, when version 2 will be release it will be on the main page. In the meanwhile please use version 1.4.0 untill version 2.0.0 is released.
 
 If you do want to use this version, keep in mind there may be bugs, parts not finished yet and untill the release not backwards-compatible.
+
+# Specifications
+
+* Transforms all HTML form elements into beautiful elements<br />
+* Allows users to use tab-index<br />
+
+# Extra included functions
+
+* String.format
+
+# Future ideas
+
+* Tri-State
--- V2/dev/jquery.fancyform.js
+++ V2/dev/jquery.fancyform.js
@@ -6,7 +6,7 @@
 * 
 * Copyright (c) 2010-2013 - Lutrasoft
 * 
-* Version: 2.2.0
+* Version: 2.0.0
 * Requires: jQuery v1.6.1+ 
 *
 * Dual licensed under the MIT and GPL licenses:
@@ -24,11 +24,20 @@
 		// Shortcuts
 		c	: "checkbox",		// 2 : checkbox
 		r	: "radio",			// 2 : radio
+		s	: "select",			// 1 : Select
 		b	: "trans-element-"	// 1 : Base class of fancyform
 	};
 	
-	function Fancyform( domEl, settings )
-	{
+	if ( !String.prototype.format ) {
+		String.prototype.format = function( ) {
+			var a = arguments;
+			return this.replace( /{(\d+)}/g, function( m, i ) { 
+				return a[i] ? a[i] : "";
+			} );
+		};
+	}
+
+	function Fancyform ( domEl, settings ) {
 		var _me 		= this,
 			_dom 		= domEl,
 			_el 		= $( _dom ).hide( ), // Make jQuery and hide
@@ -36,9 +45,10 @@
 			_settings 	= settings,
 			
 			// What kind of element are we dealing with
-			_tag 		= (_dom.tagName == "INPUT" ? _el.attr("type") : _dom.tagName)[ s.lc ]( ),
+			_tag 		= (_dom.tagName == "INPUT" ? _el.attr( "type" ) : _dom.tagName)[ s.lc ]( ),
 			_isCheckbox = _tag == s.c,
 			_isRadio 	= _tag == s.r,
+			_isSelect	= _tag == s.s,
 			
 			// Radio / Checkbox
 			_RCClasses = [ "checked", "unchecked", "disabled" ];
@@ -46,28 +56,18 @@
 		/**
 		*	Set property of an element, depending on the element it will handle the Fancyform functionality
 		*/
-		_me.prop = function( name, value, _internalCall )
-		{
+		_me.prop = function ( name, value ) {
 			// Set new value
 			_el.prop( name, value );
 			
-			// Note: Make array when more than 1
-			if( name == "checked" )
-			{
+			if( name == _RCClasses[0] ) {
 				_el.change( )
 			}
 			
 			// Update Radio or Checkbox
-			if( _isRadio || _isCheckbox )
-			{
+			if( _isRadio || _isCheckbox ) {
 				// Set right Classes
 				_me._setRCClass( );
-				
-				// Update tri-state
-				if( !_internalCall )
-				{
-					_me._tristate( );
-				}
 			}	
 		}
 		
@@ -80,70 +80,67 @@
 		/**
 		*	Handle tri-state ( :checkbox )
 		*/
-		_me._tristate = function( )
-		{	
-			if( _settings.cbr.tristate )
-			{
+		/*
+		_me._tristate = function( ) {	
+			if( _settings.cbr.tristate ) {
 				// Toggle all children to be the same as _el
-				_el.closest( "li" ).find("[type=checkbox]").fancyform( "prop", _RCClasses[ 0 ], _el.is( "checked" ), 1 );
+				_el.closest( "li" ).find("[type=checkbox]").fancyform( "prop", _RCClasses[ 0 ], _el.is( "checked" ) );
 				
 				// set right tristate class
-				$( "[name='" + _dom.name + "']" ).each(function(){
+				$( "[name='" + _dom.name + "']" ).each( function( ) {
 					var cb = $(this);
 					
-				});
+				} );
 			}
 		}
+		*/
 		
 		/**
 		*	Update the image of a :radio or :checkbox
 		*/
-		_me._setRCClass = function( )
-		{			
+		_me._setRCClass = function ( ) {			
 			_html
 				// Remove all classes
 				.removeClass( _RCClasses.join("  ") )
 				
 				// Add new class
 				.addClass( 
-						(_dom.disabled ? _RCClasses[ 2 ] : "") + " " + (_dom.checked ? _RCClasses[ 0 ] : _RCClasses[ 1 ] ) // checked || unchecked
+						(_dom.disabled ? _RCClasses[2] : "") + " " + (_dom.checked ? _RCClasses[0] : _RCClasses[1] ) // checked || unchecked
 				);
 		}
 		
 		/**
 		*	Radio-checkbox element click
 		*/
-		_me._RCElClick = function( )
-		{
+		_me._RCElClick = function ( ) {
 			// When not disabled
-			if( !_html.hasClass( _RCClasses[ 2 ] ) )
-			{
+			if( !_html.hasClass( _RCClasses[2] ) ) {
 				var checked = _el.is( ":checked" );
 				
 				// Toggle when possible
-				if( _settings.toggle || _isCheckbox || ( _isRadio && !checked ) )
-				{
-					_me.prop( _RCClasses[ 0 ], !checked, 1 ); // Checked
+				if( _settings.toggle || _isCheckbox || ( _isRadio && !checked ) ) {
+					_me.prop( _RCClasses[0], !checked ); // Checked
 				}
 				
 				// Radio needs to update all images with the same name
-				if( _isRadio )
-				{
-					$( "[name='" + _dom.name + "']" ).fancyform( "prop", _RCClasses[ 0 ], 0, 1 ); // Checked
+				if( _isRadio ) {
+					$( "[name='" + _dom.name + "']" ).fancyform( "prop", _RCClasses[0], 0 ); // Checked
 				}
 				
 				// Update tri-state
-				_me._tristate( );
+				//_me._tristate( );
 			}
 		}
 		
 		/**
 		*	Initialize :radio and :checkbox
 		*/		
-		_me._initRC = function(){
+		_me._initRC = function ( ) {
 			// Wrap with span
-			_el.wrap( "<span class='" + s.b + ( _isCheckbox ? s.c : s.r ) + "' />" ); // base + checkbox/radio
-			_html = _el.parent( );
+			_el.wrap( "<a href='#' class='" + s.b + ( _isCheckbox ? s.c : s.r ) + "' />" ); // base + checkbox/radio
+			_html = _el.parent( )
+						// Don't jump to top
+						.click( false );
 			
 			// Update image
 			_me._setRCClass( );
@@ -151,16 +148,48 @@
 			// Bind click
 			( _settings.trigger ? _el.closest( _settings.trigger ) : _html ).click( _me._RCElClick );
 		}
+
+/**
+*
+*	SELECT
+*
+*/
+
+		_me._tplGroup = function( g ) {
+			var html = "";
+			g.children( ).each( function( ) {
+				if( this.tagName == "OPTGROUP" ) {
+					html += _settings.select.tpl_group.format( this.label, _me._tplGroup( $(this) ) );
+				} else {
+					html += _settings.select.tpl_item.format( this.text );
+				}
+			} );
+			return html;
+		}
 		
+		/**
+		*	Initialize: :select
+		*/
+		_me._initS = function ( ) {
+			_html = $( _settings.select.tpl_item.format( _el.val( ), 
+						_settings.select.tpl_main.format( _me._tplGroup( _el ) ) ) 
+					);
+			
+			_el.after( _html );
+			
+		}
+
 /**
 *
 *	INIT
 *
 */
-		if( _isRadio || _isCheckbox )
-		{
+		if( _isRadio || _isCheckbox ) {
 			_me._initRC( );
-		}		
+		}
+		if( _isSelect ) {
+			_me._initS( );
+		}
 	}
 	
 	$.fancyform = {
@@ -170,10 +199,15 @@
 				trigger 	: 0, 	// null = self, if filled its .closest( VALUE ), for example LI
 
 				// Radio only
-				toggle		: 0,	// Make it possible for a radio to only select 1 but you can toggle 1 off
+				toggle		: 0		// Make it possible for a radio to only select 1 but you can toggle 1 off
 				
 				// Checkbox only
-				tristate	: 0		// Possible to use tri-state
+				//tristate	: 0		// Possible to use tri-state tree
+			},
+			select : {
+				tpl_main 	: "<ul>{0}</ul>",								// Main template, requires an A
+				tpl_item 	: "<li><a href='#'>{0}</a>{1}</li>",			// Item template
+				tpl_group	: "<ul class='group'><span>{0}</span>{1}</ul>"	// Group template
 			}
 		}
 	};
@@ -203,35 +237,27 @@
 	*	(:parent).fancyform( FNNAME )
 	*
 	*/
-	$.fn.fancyform = function( settings, value ){
+	$.fn.fancyform = function ( settings, value ){
 		return this.each( function(){
 			var _this = $( this ),
 				t, sl = _this.data( "ff" );
 			
 			// If it's an input type, go and do your job
-			if( _this.is(":input") )
-			{
-				if( sl )
-				{
+			if( _this.is(":input") ) {
+				if( sl ) {
 					t = sl[ settings ];
-					if( typeof t == "function" )
-					{
+					if( typeof t == "function" ) {
 						return t.apply( this, arguments );
 					}
-					if( value )
-					{
+					if( value ) {
 						sl.settings[ settings ] = value;
 					}
 					//return sl.settings[ settings ];
-				}
-				else
-				{
+				} else {
 					_this.data( "ff", new Fancyform( _this[0], $.extend( { }, $.fancyform.defaults, settings ) ) );
 				}
-			}
 			// Otherwise find input fields to fancyform
-			else
-			{
+			} else {
 				_this.find(":input").fancyform( settings, value );
 			}
 		} );
